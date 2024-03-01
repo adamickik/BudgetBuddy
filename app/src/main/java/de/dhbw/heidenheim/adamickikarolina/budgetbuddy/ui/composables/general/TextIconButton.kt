450fce602dccae37f92b5a1f39e6ed152e3c8e5a
adamickik <adamicki.karolina@gmail.com>
2024-03-01 18:45:47 +0100

    Changed folder structure

--- app/src/main/java/de/dhbw/heidenheim/adamickikarolina/budgetbuddy/ui/composables/general/TextIconButton.kt
+++ app/src/main/java/de/dhbw/heidenheim/adamickikarolina/budgetbuddy/ui/composables/general/TextIconButton.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -17,10 +16,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import de.dhbw.heidenheim.adamickikarolina.budgetbuddy.R
 
 @Composable
 fun TextIconButton(
