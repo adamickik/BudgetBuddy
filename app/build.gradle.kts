17292f069ee419cd71ed90869a0eb761ff0b265a
Rösch, David - TIN2021 <roeschd.tin21@student.dhbw-heidenheim.de>
2024-03-01 11:18:49 +0100

    implemented line and pie chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,13 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
 
 
+
+    implementation("androidx.compose.ui:ui:1.0.0")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha01")
+
+
+
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
