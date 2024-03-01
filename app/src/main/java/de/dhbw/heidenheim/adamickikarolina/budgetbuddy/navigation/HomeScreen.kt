c55862b3759aed1bf38bfa2e41e955ac257e028b
Marco-maier3 <maierm.tin21@student.dhbw-heidenheim.de>
2024-03-01 16:17:54 +0100

    added database for expenses

--- app/src/main/java/de/dhbw/heidenheim/adamickikarolina/budgetbuddy/navigation/HomeScreen.kt
+++ app/src/main/java/de/dhbw/heidenheim/adamickikarolina/budgetbuddy/navigation/HomeScreen.kt
@@ -10,15 +10,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Column
 
 @Preview
 @Composable
-fun HomeScreen(){
-    SavingsCard()
+fun HomeScreen() {
+    val dbHandler = DBHandler(LocalContext.current)
+
+    // Check if the database is empty before inserting test data
+    //Zum Testen, kommt später natürlich raus
+    dbHandler.insertInitialDataIfNeeded()
+
+    val expenseList = dbHandler.readExpenses()
+    Column { // This Column will arrange its children vertically
+        SavingsCard()
+        expenseList?.forEach { expense ->
+            ExpenseCard(expense)
+        }
+    }
 }
 
 
@@ -34,7 +48,7 @@ fun SavingsCard(){
     ) {
         Text(
             // TODO: change with variable from string page
-            text = "DEIN SPARDEPOT",
+            text = "Spardepot",
             modifier = Modifier
                 .padding(start = 10.dp,
                     top = 10.dp,
@@ -43,14 +57,16 @@ fun SavingsCard(){
         )
         Text(
             // TODO: change with actual value and currency
-            text = "1509,59" + " €",
+            text = "285" +  "€",
             fontWeight = FontWeight.Bold,
             fontSize = 32.sp,
             modifier = Modifier
-                .padding(start = 10.dp,
+                .padding(
+                    start = 10.dp,
                     top = 2.dp,
                     end = 10.dp,
-                    bottom = 10.dp)
+                    bottom = 10.dp
+                )
                 .align(Alignment.CenterHorizontally),
         )
         Button(
@@ -62,6 +78,42 @@ fun SavingsCard(){
             // TODO: change with text string
             Text("ZUWEISEN")
         }
+    }
+}
+
+@Composable
+fun ExpenseCard(expense: SavingsGoalModel){
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(
+            // TODO: change with variable from string page
+            text = expense.expenseName,
+            modifier = Modifier
+                .padding(start = 10.dp,
+                    top = 10.dp,
+                    end = 10.dp,
+                    bottom = 2.dp),
+        )
+        Text(
+            // TODO: change with actual value and currency
+            text = expense.expenseAmount.toString() + " €",
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            modifier = Modifier
+                .padding(
+                    start = 10.dp,
+                    top = 2.dp,
+                    end = 10.dp,
+                    bottom = 10.dp
+                )
+                .align(Alignment.End),
+        )
     }
 }
 
